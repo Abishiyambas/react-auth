c97a3631fc37b2f4968cd5deb123fa0fa0b8d01c
Abishiyam Baskaran <Abishiyam1107@icloud.com>
2026-03-18 19:55:13 +0100

    not finished

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <main className="App">
+        <Register />
+      </main>
+    </div>
+  );
+}
+
+export default App;
